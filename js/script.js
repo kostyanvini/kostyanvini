b02f21d8c5a59245d85afcf952092c118eb6dc4d
Konstantin <kostyanvi@yandex.ru>
2021-02-15 01:25:51 +0300

    Создал ToDo лист для записи задач

---
+++ js/script.js
@@ -0,0 +1,89 @@
+'use sctrict'
+//Переменные
+let todoText = document.querySelector('.todoText'),
+todoAdd = document.querySelector('.todo-add'),
+todoList = document.querySelector('.todo'),
+todo = []; //Массив со всеми задачами
+
+//Проверим, есть ли в локальном хранилеще задачи, если есть, то отрисуем сразу
+if(localStorage.getItem('task')){
+    todo = JSON.parse(localStorage.getItem('task'));
+    todoAddList();
+}
+
+//Добавление по клику
+todoAdd.addEventListener('click', addTaskInTodo)
+//Конец функции
+
+//Добавление по кнопке Enter
+todoText.addEventListener('keydown', (e) =>{
+    if(e.keyCode == 13){
+        addTaskInTodo()
+    }
+})
+//Конец функции
+
+//Функция добавления задач
+function addTaskInTodo(){
+    if(todoText.value == ""){
+        alert("Введите задачу")
+    }
+    else{
+        let newTodo = {
+            todo: todoText.value,
+            checked: false
+        }
+        todo.push(newTodo)
+        todoAddList();
+        localStorage.setItem('task', JSON.stringify(todo));    
+    }
+}
+//Конец функции
+
+//Функция отрисовки задач на экране
+function todoAddList(){
+    let todoTask = "";
+    todo.forEach(function(item, i){
+        todoTask += `
+        <div class="todo-list-item">
+            <input type="checkbox" class="task-checkbox" id="task_${i}" ${item.checked ? 'checked' : ''}>
+            <label for="task_${i}">${item.todo}</label>
+            <button class="delete-todo">Х</button>
+        </div>
+        `
+        todoList.innerHTML = todoTask;
+    })
+    todoText.value = "";
+    //Запускаем функцию после добавления новых задач
+    deleteTodoTask()
+    //Запускаем функцию после добавления новых задач
+    todoChecked()
+}
+//Конец функции
+
+//Функция удаления задач из списка
+function deleteTodoTask(){
+    let deleteTodo = document.getElementsByClassName('delete-todo');
+    for (let i = 0; i < deleteTodo.length; i++) {
+        deleteTodo[i].addEventListener('click', (e) =>{
+            e.target.parentElement.remove();
+            todo.splice(i, 1);
+            localStorage.setItem('task', JSON.stringify(todo));
+            //Запускаем потому что необходимо обновить индексы элементов в массиве deleteTodo
+            todoAddList()
+        })    
+    }
+}
+//Конец функции
+
+//Проверим и сохраним, если чекбокс активен
+function todoChecked (){
+    let todoCheckbox = document.getElementsByClassName('task-checkbox');
+    for (let i = 0; i < todoCheckbox.length; i++) {
+        todoCheckbox[i].addEventListener('change', (e) =>{
+            todo[i].checked = e.target.checked;
+            localStorage.setItem('task', JSON.stringify(todo));
+        })
+    }
+}
+//Конец функции
